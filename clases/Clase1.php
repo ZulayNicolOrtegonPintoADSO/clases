744edc153f3a57c6b0593050677b9572b365cf0c
ZulayNicolOrtegonPintoADSO <zulyortegon15@gmail.com>
2023-07-11 10:27:50 -0500

    metodo set y get

--- clases/Clase1.php
+++ clases/Clase1.php
@@ -2,33 +2,64 @@
     class Clase1{
         // 1.Atributos con modificadores de acceso
         // private, public, protected
-        public $nombre;
-        public $email ;
-        public $celular ;
+        private $nombre;
+        private $email ;
+        private $celular ;
 
         //2. Contructor (metodo magico) 
         // Un método es una función que está dentro de una clase
         // ideal para iniciar valores de propiedades
         // Palabra reservada this (Objeto actual)
         // En ocasiones se inicializa con valores parametrizados
-        public function __construct($nombre , $email, $celular){ 
-            $this-> nombre = $nombre;
-            $this-> email = $email ;
-            $this-> celular = $celular ;
-        }
 
+
+        // public function __construct($nombre , $email, $celular){ 
+        //     $this-> nombre = $nombre;
+        //     $this-> email = $email ;
+        //     $this-> celular = $celular ;
+        // }
+
+        
         // 3.metodos (Getters and Setter)
         // get -> obtener valores de Atributos
-        // set -> modificar valores de atributos
+        public function getNombre(){
+            return $this->nombre;
+        }
 
+        public function getEmail(){
+            return $this->email;
+        }
+
+        public function getCelular(){
+            return $this->celular;
+        }
+
+        // set -> modificar valores de atributos
         // UN OBJETO ES LA INSTANCIA DE LA CLASE
-        
+        public function setNombre($nombre){
+            $this->nombre = $nombre;
+        }
+
+        public function setEmail($email){
+            $this->email = $email;
+        }
+
+        public function setCelular($celular){
+            $this->celular = $celular;
+        }
+     
     }
     
     // Instancia de la clase (OBJETO) --> Variable de instancia
-    $clases = new Clase1("Hola" , "Email" , 21321);
-    echo $clases->nombre . "<br>";
-    echo $clases->email . "<br>" ;
-    echo $clases->celular;
+    // $clases = new Clase1("Hola" , "Email" , 21321);
+    $clases = new Clase1;
+
+    $clases->setNombre("Siu");
+    $clases->setEmail("correo");
+    $clases->setCelular(21321);
+
+    echo $clases->getNombre() . "<br>";
+    echo $clases->getEmail() . "<br>" ;
+    echo $clases->getCelular();
 
 ?>
